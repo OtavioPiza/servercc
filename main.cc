897d2e7c330dbbe5b0bab7e4ab3ea3a53fb23b75
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2022-05-05 18:03:10 -0400

    testing

--- main.cc
+++ main.cc
@@ -2,12 +2,21 @@
 #include "server/src/server.hh"
 #include "router/src/router.hh"
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 	restpp::Server s(8080, THREAD_MODE);
 
-	s.router.handle("GET", "/", [](int, int) {
-		return true;
-	});
+	s.router.handle("GET", "/test", [](int, int)
+					{ return true; });
+
+	s.router.handle("GET", "/", [](int, int)
+					{ return true; });
+
+	s.router.handle("GET", "/test/hello", [](int, int)
+					{ return true; });
+
+	s.router.handle("GET", "/test/hello/world", [](int, int)
+					{ return true; });
 
 	s.run();
 	return 0;
