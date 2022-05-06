e5507129d1aa8263332b12a65575f37647067275
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2022-05-05 20:08:21 -0400

    included

--- router/handler/src/handler.hh
+++ router/handler/src/handler.hh
@@ -5,18 +5,21 @@
 #include <string>
 #include <utility>
 
+#include "../../request/src/request.hh"
+#include "../../response/src/response.hh"
+
 namespace restpp
 {
 
     class Handler
     {
     public:
-        bool (*handle_get)(int, int);                      // Handler for GET requests
-        bool (*handle_post)(int, int);                     // Handler for POST requests
-        bool (*handle_put)(int, int);                      // Handler for PUT requests
-        bool (*handle_patch)(int, int);                    // Handler for PATCH requests
-        bool (*handle_delete)(int, int);                   // Handler for DELETE requests
-        bool (*handle_options)(int, int);                  // Handler for OPTIONS requests
+        bool (*handle_get)(Response &, Response &);        // Handler for GET requests
+        bool (*handle_post)(Response &, Response &);       // Handler for POST requests
+        bool (*handle_put)(Response &, Response &);        // Handler for PUT requests
+        bool (*handle_patch)(Response &, Response &);      // Handler for PATCH requests
+        bool (*handle_delete)(Response &, Response &);     // Handler for DELETE requests
+        bool (*handle_options)(Response &, Response &);    // Handler for OPTIONS requests
         std::vector<std::pair<std::string, Handler>> next; // Next handlers
 
         /**
