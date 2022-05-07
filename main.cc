1ccf01a00570c2cc6bd13f9409ccb391311f0623
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2022-05-06 20:42:09 -0400

    wip

--- main.cc
+++ main.cc
@@ -1,15 +1,13 @@
 #include <bits/stdc++.h>
 
 #include "server/src/server.hh"
-#include "response/src/request.hh"
 #include "response/src/response.hh"
 
 int main(int argc, char **argv)
 {
 	restpp::Server s(8080, THREAD_MODE);
 
-	s.router.handle("GET", "/", [](restpp::Request &req, restpp::Response &res) {
-
+	s.router.handle(GET, "/", [](restpp::Request &req, restpp::Response &res) {
 		
 	});
 
