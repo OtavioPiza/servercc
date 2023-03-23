97a78584aea66e243fecd8d35c7e175f3b3efc91
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2023-03-23 14:03:31 -0400

    done

--- src/client/src/udp_client/src/udp_client.cc
+++ src/client/src/udp_client/src/udp_client.cc
@@ -20,7 +20,6 @@ UdpClient::UdpClient(const std::string interface, const std::string server_addre
       multicast_group(multicast_group){};
 
 StatusOr<bool> UdpClient::open_socket() {
-
     // If the socket is already open, return.
     if (is_socket_open) {
         return StatusOr<bool>(Status::SUCCESS, "Socket is already open.", true);
@@ -74,7 +73,7 @@ StatusOr<bool> UdpClient::open_socket() {
 
     // Set the socket address.
     struct sockaddr_in socket_address;
-    memset(&socket_address, 0, sizeof(socket_address)); 
+    memset(&socket_address, 0, sizeof(socket_address));
     socket_address.sin_family = AF_INET;
     socket_address.sin_addr.s_addr = inet_addr(get_address().c_str());
     socket_address.sin_port = htons(get_port());
@@ -82,5 +81,18 @@ StatusOr<bool> UdpClient::open_socket() {
     // Set the socket file and return.
     client_fd = socket_fd;
     is_socket_open = true;
+
+    // Broadcast a message to the server.
+    std::string message = "Hello, server!";
+    if (sendto(client_fd, message.c_str(), message.size(), 0, (struct sockaddr *)&socket_address,
+               sizeof(socket_address)) < 0) {
+        perror("sendto");
+        close(client_fd);
+        return StatusOr<bool>(Status::ERROR, "Failed to send message to server.", 0);
+    }
+
     return StatusOr<bool>(Status::SUCCESS, "Socket opened successfully.", true);
 }
+
+/// See udp_client.h for documentation.
+StatusOr<bool> UdpClient::close_socket() {};
